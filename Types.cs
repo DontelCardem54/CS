78a9b4c032058cefce58f0808d5a75bf9e6c4ff9
DontelCardem54 <chernykhvalentin@gmail.com>
2023-07-29 15:29:16 +0300

    Create Types.cs

---
+++ Types.cs
@@ -0,0 +1,13 @@
+﻿namespace FreeWeekTG
+{
+    internal class Types
+    {
+        short a = -99;
+        int b = 55555;
+        long c = 2394672295;
+        double d = 65.46;
+        char e = 'a';
+        string f = "World";
+
+    }
+}
