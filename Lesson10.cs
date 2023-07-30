ac986ce197402bd5cf685fcefcf9215f33b6932f
DontelCardem54 <chernykhvalentin@gmail.com>
2023-07-30 15:10:43 +0300

    Create Lesson10.cs

---
+++ Lesson10.cs
@@ -0,0 +1,19 @@
+﻿bool[] thimbles = new bool[3] { false, false, true };
+
+Console.WriteLine("Игра в наперстки");
+Console.WriteLine();
+Console.WriteLine("под одним из трех наперстков находится шарик. Попробуйте угадать в каком!");
+Console.WriteLine();
+Console.Write("Введите ваше число от 1 до 3: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+bool result = thimbles[input-1];
+
+if (result == true)
+{
+    Console.WriteLine("Да! Вы нашли шарик!");
+}
+else
+{
+    Console.WriteLine("Нет! Вы не угадали!");
+}
