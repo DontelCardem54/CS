8540957bf6b78dd8a2de54c6d1326e72152f0909
DontelCardem54 <chernykhvalentin@gmail.com>
2023-07-30 15:42:51 +0300

    Create Lesson11.cs

---
+++ Lesson11.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
+
+Console.WriteLine("Квадраты чисел");
+Console.WriteLine();
+Console.Write("Дан ряд чисел: ");
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{i} ");
+}
+
+Console.WriteLine();
+Console.Write("Квадраты этих чисел: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{i * i} ");
+}
