baab2bd24a06429d0fc6ab7aea49b769103e7806
DontelCardem54 <chernykhvalentin@gmail.com>
2023-07-31 09:11:14 +0300

    Update Lesson11.cs

--- Lesson11.cs
+++ Lesson11.cs
@@ -1,12 +1,14 @@
-﻿int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
+﻿using System;
+
+int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
 
 Console.WriteLine("Квадраты чисел");
 Console.WriteLine();
 Console.Write("Дан ряд чисел: ");
 
-for(int i = 0; i < numbers.Length; i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    Console.Write($"{i} ");
+    Console.Write($"{numbers[i]} ");
 }
 
 Console.WriteLine();
@@ -14,5 +16,5 @@ Console.Write("Квадраты этих чисел: ");
 
 for (int i = 0; i < numbers.Length; i++)
 {
-    Console.Write($"{i * i} ");
+    Console.Write($"{numbers[i] * numbers[i]} ");
 }
